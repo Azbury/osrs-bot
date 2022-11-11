f16467b5c532218d3fa5854ee668eff1d564716b
Austin Asbury <acasbury25@aol.com>
2022-11-11 10:18:21 -0500

    find goblin grey color

--- index.js
+++ index.js
@@ -7,22 +7,27 @@ function main() {
     while (true) {
         var goblin = findGoblin();
 
-        robot.moveMouse(tree.x, tree.y);
+        if (goblin === false) {
+            console.log("Could not find goblin");
+            break;
+        }
+
+        robot.moveMouse(goblin.x, goblin.y);
         robot.mouseClick();
 
-        sleep(10000);
+        sleep(30000);
     }
 
     console.log("done.");
 }
 
 function findGoblin() {
-    var x = 300, y = 300, width = 1300, height = 400;
+    var x = 300, y = 300, width = 1300, height = 500;
     var img = robot.screen.capture(x, y, width, height);
 
-    var goblin_colors = ["635949", "341f03", "9d908f", "676c1c", "5a3e06", "574e40"];
+    var goblin_colors = ["574e40", "595042", "635949", "736855", "605747", "5e5545", "5c5244", "655b4b", "736855"];
 
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 2000; i++) {
         var random_x = getRandomInt(0, width - 1);
         var random_y = getRandomInt(0, height - 1);
         var sample_color = img.colorAt(random_x, random_y);
@@ -35,6 +40,8 @@ function findGoblin() {
             return { x: screen_x, y: screen_y };
         }
     }
+
+    return false;
 }
 
 function getRandomInt(min, max) {
